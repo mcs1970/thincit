3f9d1daefc25814aef8b9a42dc486bdf96c1737b
Kevin D Williams <46939463+mcs1970@users.noreply.github.com>
2023-08-04 14:20:48 -0400

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # install apache
-sudo apt-get -y clean
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install apache2
+sudo a2enmod ssl
 
 # enable apache Full config
 sudo ufw allow 'Apache Secure'
-
